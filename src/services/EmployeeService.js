7162a0683a0bb0b7737954a9453b98a8943e043a
Uttam Kumar <uttam.kr.work@gmail.com>
2021-10-06 18:12:47 +0530

    make it better

--- src/services/EmployeeService.js
+++ src/services/EmployeeService.js
@@ -1,7 +1,7 @@
 
 import axios from 'axios';
 
-const EMPLOYEE_API_BASE_URL="http://localhost:8091";
+const EMPLOYEE_API_BASE_URL="https://webapp-ems.herokuapp.com";
 
 class EmployeeService{
     getEmployees(){
